a7878e7b7d6fcef974cf9043b5a8b816808513ab
Jefferson <U20215320@utp.edu.pe>
2022-10-31 17:54:19 -0500

    Conexion a API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
   
+  const [criptos, setCriptos] = useState();
+
+
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_URL}assets`)
+      .then( data => {
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.error('la petición fallo')
+      })
+
+  }, [])
+
+  if(!criptos){return (<span>Cargando...</span>)}
 
   return (
-    <h1>Hola mundo</h1>
+    <Fragment>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {criptos.map( cripto => 
+          <li key={cripto.id} >Nombre: {cripto.name} Precio: {cripto.priceUsd}</li>
+        )}
+      </ol>
+    </Fragment>
   )
 }
 
-export default App
+export  {App}
